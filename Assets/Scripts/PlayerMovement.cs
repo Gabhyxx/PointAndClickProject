8ba6ecc0fa28019b5dd943cedb19c814ab74e359
JuanJoseRodriguezdePraAviles <jjpra196@gmail.com>
2024-04-02 17:32:44 +0200

    Rotation to playyer added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] Rigidbody rb;
     [SerializeField] float speed = 5;
+    [SerializeField] float rotationSpeed = 1;
     [SerializeField] float maxForce;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -18,28 +19,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
         Vector3 currentVelocity = rb.velocity;
         Vector3 targetVelocity = new Vector3(move.x, 0, move.y);
         targetVelocity *= speed;
-
-        targetVelocity = transform.TransformDirection(targetVelocity);
-
+        
+        
         Vector3 velocityChange = (targetVelocity - currentVelocity);
 
         Vector3.ClampMagnitude(velocityChange, maxForce);
 
-
-        rb.AddForce(velocityChange, ForceMode.VelocityChange);
-    }
-
-    void Start()
-    {
         
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        
+        
+        if(currentVelocity != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(targetVelocity, Vector3.up);
+
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, toRotation, rotationSpeed *  Time.deltaTime);
+        }
     }
-
-    void Update()
-    {
-    }
-
-
 }
